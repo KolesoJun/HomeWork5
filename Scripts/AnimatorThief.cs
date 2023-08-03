7f251300beea42d9bee89dfe307864bcbe5023a6
KolesoJun <danil187crr@yandex.ru>
2023-08-03 15:52:23 +0300

    com

---
+++ Scripts/AnimatorThief.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AnimatorThief
+{
+    public static class States
+    {
+        
+    }
+
+    public static class Parametrs
+    {
+        public const string Speed = "Speed";
+        public const string Jump = "Jump";
+        public const string IsNotGround = "IsNotGround";
+    }
+}
